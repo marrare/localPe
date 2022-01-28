e1b4b5913ace3e32c2cf04d29276da08d8db3ebb
marrare <rivaldo.alvesb36@gmail.com>
2022-01-28 09:25:13 -0300

    tela cadastro lugar

--- localpe-mobile/App.js
+++ localpe-mobile/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import Cadastro from './screens/cadastro';
-import HomeScreen from './screens/homeScreen';
-import Login from './screens/login';
-import UserLogado from './screens/userLogado';
-import DetalharLugar from './screens/detalharLugar';
+import CadastroScreen from './screens/cadastro/cadastroScreen';
+import HomeScreen from './screens/home/homeScreen';
+import LoginScreen from './screens/login/loginScreen';
+import UserLogadoScreen from './screens/userLogadoScreen';
+import DetalheLugarScreen from './screens/lugar/detalhe/detalheLugarScreen';
+import CadastroLugarScreen from './screens/lugar/cadastro/cadastroLugarScreen';
 
 
 
@@ -15,13 +16,13 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="inicio">
-        <Stack.Screen name="detalharLugar" component={DetalharLugar} options={{ headerShown: false }} />
-        <Stack.Screen name="userLogado" component={UserLogado} options={{ headerShown: false }} />
-        <Stack.Screen name="login" component={Login} options={{ headerShown: false }} />
+      <Stack.Navigator initialRouteName="cadastroLugar">
+        <Stack.Screen name="detalharLugar" component={DetalheLugarScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="userLogado" component={UserLogadoScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="login" component={LoginScreen} options={{ headerShown: false }} />
         <Stack.Screen name="inicio" component={HomeScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="cadastro" component={Cadastro} options={{ headerShown: false }} />
-
+        <Stack.Screen name="cadastro" component={CadastroScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="cadastroLugar" component={CadastroLugarScreen} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
