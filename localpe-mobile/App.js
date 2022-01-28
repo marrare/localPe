24fd2901b73834e56e006b631c4d27c2140221d4
roberta976 <robertavitoria976@gmail.com>
2022-01-27 23:30:10 -0300

    atualizacao

--- localpe-mobile/App.js
+++ localpe-mobile/App.js
@@ -9,17 +9,19 @@ import UserLogado from './screens/userLogado';
 import DetalharLugar from './screens/detalharLugar';
 
 
+
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="inicio">
-      <Stack.Screen name="detalharLugar" component={DetalharLugar} options={{ headerShown: false }} />
+        <Stack.Screen name="detalharLugar" component={DetalharLugar} options={{ headerShown: false }} />
         <Stack.Screen name="userLogado" component={UserLogado} options={{ headerShown: false }} />
         <Stack.Screen name="login" component={Login} options={{ headerShown: false }} />
         <Stack.Screen name="inicio" component={HomeScreen} options={{ headerShown: false }} />
         <Stack.Screen name="cadastro" component={Cadastro} options={{ headerShown: false }} />
+
       </Stack.Navigator>
     </NavigationContainer>
   );
